65527dac1373e7fe070921363e3c7cd62864fbee
22520187 <22520187@gm.uit.edu.vn>
2025-09-19 22:32:50 +0700

    feat: profile component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,12 @@ export { default as FilterTabs } from './FilterTabs/FilterTabs';
 export { default as QuestionCard } from './QuestionCard/QuestionCard';
 export { default as QuestionList } from './QuestionList/QuestionList';
 export { default as FloatingActionButton } from './FloatingActionButton/FloatingActionButton';
+
+// Profile components
+export { default as ProfileSection } from './Account/ProfileSection';
+export { default as ProfileAvatar } from './Account/ProfileAvatar';
+export { default as EditableSection } from './Account/EditableSection';
+export { default as EditableField } from './Account/EditableField';
+export { default as GenderSection } from './Account/GenderSection';
+export { default as DateOfBirthSection } from './Account/DateOfBirthSection';
+export { default as PasswordSection } from './Account/PasswordSection';
